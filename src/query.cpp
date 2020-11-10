984e87e477825421507622db47cf70bd23f75a4b
Retro <44505837+dankmolot@users.noreply.github.com>
2020-11-10 08:21:43 +0200

    query too low perfomance fix.

--- src/query.cpp
+++ src/query.cpp
@@ -38,24 +38,38 @@ int Query::META;
 std::mutex Query::LuaMutex;
 std::queue<std::function<void(Lua::ILuaBase*)>> Query::LuaQueue;
 
-std::mutex Query::ThreadMutex;
-std::condition_variable Query::ThreadCVar;
-bool Query::ThreadStopped;
-std::queue<SmartPointer<Query>*> Query::ThreadQueue;
+//std::mutex Query::ThreadMutex;
+//std::condition_variable Query::ThreadCVar;
+//bool Query::ThreadStopped;
+//std::queue<SmartPointer<Query>*> Query::ThreadQueue;
+//std::promise<SmartPointer<Query>*> Query::ThreadPromise;
+//std::future<SmartPointer<Query>*> Query::ThreadFuture;
 
 void Query::New(Lua::ILuaBase* LUA, std::function<void(Query*)> func)
 {
 	auto ptr = new SmartPointer<Query>;
-	auto q = ptr->guard(); ptr->add();
+	auto q = ptr->guard();
 	q->m_func = func;
 	q->lua_object = LUA;
 
+	ptr->add();
 	std::thread([ptr] {
-		std::lock_guard<std::mutex> guard(ThreadMutex);
-		ThreadQueue.push(ptr);
-		ThreadCVar.notify_one();
-	}).detach();
+		DevMsg(1, "New query: 0x%p\n", ptr);
+		if (ptr == nullptr)
+			return;
 
+		auto q = ptr->guard(false);
+		if (!q->lua_object)
+			return;
+
+		DevMsg(1, "Query is valid. launching func...\n");
+		q->m_func(ptr->get());
+
+		DevMsg(1, "Sending end message...\n");
+		std::lock_guard<std::mutex> guard(q->m_mtx);
+		q->m_done = true;
+		q->m_cvar.notify_one();
+	}).detach();
 	LUA->PushUserType(ptr, META);
 }
 
@@ -82,39 +96,61 @@ int Query::Wait(lua_State* L)
 	return 0;
 }
 
-void Query::ThreadFunc()
-{
-	DevMsg(1, "Query thread started!");
-	do {
-		std::unique_lock<std::mutex> lck{ThreadMutex};
-		ThreadCVar.wait(lck, [&] {
-			return ThreadStopped || !ThreadQueue.empty();
-		});
-
-		while (!ThreadQueue.empty()) {
-			auto ptr = ThreadQueue.front();
-			ThreadQueue.pop();
-
-			if (!ptr)
-				continue;
-
-			auto q = ptr->guard(false);
-			if (!q->lua_object)
-				continue;
-
-			q->m_func(ptr->get());
-
-			std::lock_guard<std::mutex> guard(q->m_mtx);
-			q->m_done = true;
-			q->m_cvar.notify_one();
-		}
-
-		if (ThreadStopped) {
-			DevMsg(1, "Query thread stopped!");
-			break;
-		}
-	} while(true);
-}
+//void Query::ThreadFunc()
+//{
+//	DevMsg(1, "Query thread handler started!\n");
+//
+//	while (!ThreadStopped) {
+//		auto ptr = ThreadFuture.get();
+//		DevMsg(1, "New query: 0x%p\n", ptr);
+//		if (ptr == nullptr)
+//			continue;
+//
+//		auto q = ptr->guard(false);
+//		if (!q->lua_object)
+//			continue;
+//
+//		DevMsg(1, "Query is valid. launching func...\n");
+//		q->m_func(ptr->get());
+//
+//		DevMsg(1, "Sending end message...\n");
+//		std::lock_guard<std::mutex> guard(q->m_mtx);
+//		q->m_done = true;
+//		q->m_cvar.notify_one();
+//	}
+//
+//	DevMsg(1, "Query thread handler stopped!\n");
+//	//DevMsg(1, "Query thread started!");
+//	//do {
+//	//	std::unique_lock<std::mutex> lck{ThreadMutex};
+//	//	ThreadCVar.wait(lck, [&] {
+//	//		return ThreadStopped || !ThreadQueue.empty();
+//	//	});
+//
+//	//	while (!ThreadQueue.empty()) {
+//	//		auto ptr = ThreadQueue.front();
+//	//		ThreadQueue.pop();
+//
+//	//		if (!ptr)
+//	//			continue;
+//
+//	//		auto q = ptr->guard(false);
+//	//		if (!q->lua_object)
+//	//			continue;
+//
+//	//		q->m_func(ptr->get());
+//
+//	//		std::lock_guard<std::mutex> guard(q->m_mtx);
+//	//		q->m_done = true;
+//	//		q->m_cvar.notify_one();
+//	//	}
+//
+//	//	if (ThreadStopped) {
+//	//		DevMsg(1, "Query thread stopped!");
+//	//		break;
+//	//	}
+//	//} while(true);
+//}
 
 int Query::Think(lua_State* L)
 {
@@ -152,12 +188,18 @@ void Query::Initialize(Lua::ILuaBase* LUA)
 		LUA->Pop();
 	LUA->Pop();
 
-	std::thread(&Query::ThreadFunc).detach();
+	//ThreadFuture = ThreadPromise.get_future();
+	//std::thread(&ThreadFunc).detach();
+	//std::thread(&Query::ThreadFunc).detach();
 }
 
 void Query::Deinitialize()
 {
-	std::lock_guard<std::mutex> guard(ThreadMutex);
-	ThreadStopped = true;
-	ThreadCVar.notify_one();
+
+	//ThreadStopped = true;
+	//ThreadPromise.set_value(nullptr);
+
+	//std::lock_guard<std::mutex> guard(ThreadMutex);
+	//ThreadStopped = true;
+	//ThreadCVar.notify_one();
 }
